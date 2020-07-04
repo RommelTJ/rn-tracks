e7a979163aa5212bc7b9da0e2e38975168860dfb
Rommel Rico <me@rommelrico.com>
2020-07-04 12:52:23 -0700

    :sparkles: Tracks: Navigate to TrackDetail

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const TrackListScreen = () => {
+const TrackListScreen = (props) => {
+  const { navigation } = props;
   return (
-    <View>
+    <>
       <Text style={{fontSize: 48}}>Track List Screen</Text>
-    </View>
+      <Button title="Go to Track Detail" onPress={() => navigation.navigate("TrackDetail")} />
+    </>
   );
 };
 
