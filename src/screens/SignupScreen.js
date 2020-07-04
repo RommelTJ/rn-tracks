432f449e88f94481466be711e6cfe99e3b184106
Rommel Rico <me@rommelrico.com>
2020-07-04 13:06:05 -0700

    :sparkles: Tracks: Using the Spacer

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
+import Spacer from "../components/Spacer";
 
 const SignupScreen = (props) => {
   const { navigation } = props;
 
   return (
     <>
-      <Text h3>Sign Up for Tracker</Text>
-      <Input label="Email" />
-      <Input label="Password" />
-      <Button title="Sign Up" />
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Spacer>
+        <Input label="Email" />
+        <Input label="Password" />
+      </Spacer>
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
     </>
   );
 };
