b3977cc5bdeba1fc3a461eb8f9208d5767d958b8
Rommel Rico <me@rommelrico.com>
2020-07-04 12:50:02 -0700

    :sparkles: Tracks: Navigate to main flow

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -8,6 +8,7 @@ const SignupScreen = (props) => {
     <>
       <Text style={{fontSize: 48}}>Sign Up Screen</Text>
       <Button title="Go to SignIn" onPress={() => navigation.navigate("Signin")} />
+      <Button title="Go to Main Flow" onPress={() => navigation.navigate("mainFlow")} />
     </>
   );
 };
