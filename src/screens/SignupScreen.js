c42da63a596e7e92979efca06e170c4e24b02501
Rommel Rico <me@rommelrico.com>
2020-07-04 12:49:07 -0700

    :sparkles: Tracks: Navigate to Signin

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const SignupScreen = () => {
+const SignupScreen = (props) => {
+  const { navigation } = props;
+
   return (
-    <View>
+    <>
       <Text style={{fontSize: 48}}>Sign Up Screen</Text>
-    </View>
+      <Button title="Go to SignIn" onPress={() => navigation.navigate("Signin")} />
+    </>
   );
 };
 
