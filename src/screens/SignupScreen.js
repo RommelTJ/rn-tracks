46821e4367057dca627f1c9f497b0c153f36c004
Rommel Rico <me@rommelrico.com>
2020-07-04 13:11:11 -0700

    :lipstick: Tracks: Container styling

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from "../components/Spacer";
 
@@ -7,7 +7,7 @@ const SignupScreen = (props) => {
   const { navigation } = props;
 
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
@@ -18,7 +18,7 @@ const SignupScreen = (props) => {
       <Spacer>
         <Button title="Sign Up" />
       </Spacer>
-    </>
+    </View>
   );
 };
 
@@ -28,6 +28,12 @@ SignupScreen.navigationOptions = () => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200
+  }
+});
 
 export default SignupScreen;
