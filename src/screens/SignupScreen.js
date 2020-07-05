f4f577ac4a238f7d37ab41e08b49388b896409d6
Rommel Rico <me@rommelrico.com>
2020-07-05 15:25:19 -0700

    :sparkles: Tracks: Implementing the signup action

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const SignupScreen = (props) => {
   const { navigation } = props;
+  const { state, signup } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -31,7 +33,7 @@ const SignupScreen = (props) => {
         />
       </Spacer>
       <Spacer>
-        <Button title="Sign Up" />
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
       </Spacer>
     </View>
   );
