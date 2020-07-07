73a1f33bab97dd1c56e3cf3409e9b90d9e073bd2
Rommel Rico <me@rommelrico.com>
2020-07-07 06:44:11 -0700

    :tada: Tracks: AuthForm

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,41 +1,22 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
+import { Text } from 'react-native-elements';
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
 
 const SignupScreen = (props) => {
   const { navigation } = props;
   const { state, signup } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Spacer>
-        <Input
-          autoCapitalize="none"
-          autoCorrect={false}
-          label="Email"
-          value={email}
-          onChangeText={setEmail}
-        />
-        <Input
-          autoCapitalize="none"
-          autoCorrect={false}
-          secureTextEntry
-          label="Password"
-          value={password}
-          onChangeText={setPassword}
-        />
-      </Spacer>
-      { state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : undefined }
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signup({ email, password })} />
-      </Spacer>
+      <AuthForm
+        headerText={"Sign Up for Tracker"}
+        errorMessage={state.errorMessage}
+        submitButtonText={"Sign Up"}
+        onSubmit={signup}
+      />
       <Spacer>
         <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
           <Text style={styles.link}>Already have an account? Sign in instead.</Text>
@@ -56,11 +37,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     marginBottom: 200
-  },
-  errorMessage: {
-    fontSize: 16,
-    color: 'red',
-    marginHorizontal: 15
   },
   link: {
     color: 'blue'
