7ac9657c1a982a4e20a9492493e12277b3480b52
Rommel Rico <me@rommelrico.com>
2020-07-07 06:57:23 -0700

    :sparkles: Tracks: Signin screen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
+import {Context as AuthContext} from "../context/AuthContext";
 
 const SigninScreen = () => {
+  const { state, signin } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text style={{fontSize: 48}}>Sign In Screen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText={"Sign In To Your Account"}
+        errorMessage={state.errorMessage}
+        submitButtonText={"Sign In"}
+        onSubmit={signin}
+      />
+      <NavLink title="Don't have an account? Sign up instead." routeName="Signup" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200
+  }
+});
 
 export default SigninScreen;
