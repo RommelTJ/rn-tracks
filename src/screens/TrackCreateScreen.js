648ee206bdcfd682326ac1e9add5658ced55e78b
Rommel Rico <me@rommelrico.com>
2020-07-09 07:09:57 -0700

    :sparkles: Tracks: Using Map in TrackCreateScreen

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
 import { SafeAreaView } from "react-navigation";
+import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
-      <Text style={{fontSize: 48}}>Track Create Screen</Text>
+      <Text h2>Create a Track</Text>
+      <Map />
     </SafeAreaView>
   );
 };
