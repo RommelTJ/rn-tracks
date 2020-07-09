0b477e9bc85fb2e354eb507183cdc8bdc87164ae
Rommel Rico <me@rommelrico.com>
2020-07-09 07:20:12 -0700

    :sparkles: Tracks: Drawing a series of points

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { StyleSheet } from "react-native";
-import { Text } from 'react-native-elements';
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
+  const initialRegion = { latitude: 37.33233, longitude: -122.03121, latitudeDelta: 0.01, longitudeDelta: 0.01 };
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({latitude: initialRegion.latitude + i * 0.001, longitude: initialRegion.longitude + i * 0.001});
+    } else {
+      points.push({latitude: initialRegion.latitude - i * 0.002, longitude: initialRegion.longitude - i * 0.002});
+    }
+  }
+
   return (
-    <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.33233,
-          longitude: -122.03121,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01
-        }}
-    />
+    <MapView style={styles.map} initialRegion={initialRegion}>
+      <Polyline coordinates={points} />
+    </MapView>
   );
 };
 
