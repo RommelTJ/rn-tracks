c089b3947a21cfaf9b8ae4d69378d4599e5d7f1c
Rommel Rico <me@rommelrico.com>
2020-07-09 07:07:34 -0700

    :tada: Tracks: Map

---
+++ src/components/Map.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet } from "react-native";
+import { Text } from 'react-native-elements';
+
+const Map = () => {
+  return (
+    <>
+      <Text>I am a map</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Map;
