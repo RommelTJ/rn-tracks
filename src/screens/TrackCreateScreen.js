bb6e5ebe362eeee420f2f1e8cb84520cc091fc56
Rommel Rico <me@rommelrico.com>
2020-07-10 07:12:18 -0700

    :sparkles: Tracks: Requesting Location Permissions

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from "react-navigation";
+import { requestPermissionsAsync } from "expo-location";
+
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
+  const [error, setError] = useState(undefined);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (e) {
+      setError(e);
+    }
+  };
+
+  useEffect(() => { startWatching().then(); }, []);
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>
       <Map />
+      { error ? <Text>Please enable location services</Text> : undefined }
     </SafeAreaView>
   );
 };
