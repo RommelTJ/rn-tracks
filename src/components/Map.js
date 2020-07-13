188e0553c3ac93b1ee2e3b4f1aa7ca7c1e1c1064
Rommel Rico <me@rommelrico.com>
2020-07-12 17:58:02 -0700

    :sparkles: Tracks: Drawing a Position Indicator

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { StyleSheet, ActivityIndicator } from "react-native";
-import MapView, { Polyline } from "react-native-maps";
+import MapView, { Polyline, Circle } from "react-native-maps";
 
 import { Context as LocationContext } from "../context/LocationContext";
 
@@ -10,8 +10,13 @@ const Map = () => {
   if (!currentLocation) return <ActivityIndicator size="large" style={{ marginTop: 200}} />;
   const initialRegion = { ...currentLocation.coords, latitudeDelta: 0.01, longitudeDelta: 0.01 };
   return (
-    <MapView style={styles.map} initialRegion={initialRegion} region={initialRegion}>
-      
+    <MapView style={styles.map} initialRegion={initialRegion}>
+      <Circle
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
     </MapView>
   );
 };
