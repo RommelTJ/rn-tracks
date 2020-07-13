99be9b83d2876c0f8e7f5d2739ffedf27635655f
Rommel Rico <me@rommelrico.com>
2020-07-12 17:52:56 -0700

    :sparkles: Tracks: Passing location to Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import { StyleSheet } from "react-native";
+import React, { useContext } from 'react';
+import { StyleSheet, ActivityIndicator } from "react-native";
 import MapView, { Polyline } from "react-native-maps";
 
-const Map = () => {
-  const initialRegion = { latitude: 37.33233, longitude: -122.03121, latitudeDelta: 0.01, longitudeDelta: 0.01 };
-  let points = [];
-  for (let i = 0; i < 20; i++) {
-    if (i % 2 === 0) {
-      points.push({latitude: initialRegion.latitude + i * 0.001, longitude: initialRegion.longitude + i * 0.001});
-    } else {
-      points.push({latitude: initialRegion.latitude - i * 0.002, longitude: initialRegion.longitude - i * 0.002});
-    }
-  }
+import { Context as LocationContext } from "../context/LocationContext";
 
+const Map = () => {
+  const { state: { currentLocation } } = useContext(LocationContext);
+
+  if (!currentLocation) return <ActivityIndicator size="large" style={{ marginTop: 200}} />;
+  const initialRegion = { ...currentLocation.coords, latitudeDelta: 0.01, longitudeDelta: 0.01 };
   return (
-    <MapView style={styles.map} initialRegion={initialRegion}>
-      <Polyline coordinates={points} />
+    <MapView style={styles.map} initialRegion={initialRegion} region={initialRegion}>
+      
     </MapView>
   );
 };
