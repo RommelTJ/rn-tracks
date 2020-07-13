861443c51311083ccd02eae30a364b5ac2d40ac1
Rommel Rico <me@rommelrico.com>
2020-07-13 07:22:03 -0700

    :sparkles: Tracks: Start and Stop Recording actions

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -4,14 +4,22 @@ const locationReducer = (state, action) => {
   switch (action.type) {
     case 'add_current_location':
       return { ...state, currentLocation: action.payload };
+    case 'start_recording':
+      return { ...state, recording: true };
+    case 'stop_recording':
+      return { ...state, recording: false };
     default:
       return state;
   }
 };
 
-const startRecording = (dispatch) => () => {};
+const startRecording = (dispatch) => () => {
+  dispatch({ type: 'start_recording' });
+};
 
-const stopRecording = (dispatch) => () => {};
+const stopRecording = (dispatch) => () => {
+  dispatch({ type: 'stop_recording' });
+};
 
 const addLocation = (dispatch) => (location) => {
   dispatch({ type: 'add_current_location', payload: location });
