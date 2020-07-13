adeb1b1f1a7e558ead9511227cf3b4d71c1391a0
Rommel Rico <me@rommelrico.com>
2020-07-13 07:19:13 -0700

    :lipstick: Tracks: Adding spacing to form

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Input, Button } from 'react-native-elements';
 
+import Spacer from "./Spacer";
+
 const TrackForm = () => {
   return (
     <>
-      <Input />
+      <Spacer>
+        <Input placeholder="Enter name" />
+      </Spacer>
       <Button title="Start Recording" />
     </>
   );
