cc03b2a6b406b8114f2d3a1e5ae3c25f5941032f
Rommel Rico <me@rommelrico.com>
2020-07-15 07:25:03 -0700

    :sparkles: Tracks: Tip for avoiding stale references

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -3,34 +3,35 @@ import { Accuracy, requestPermissionsAsync, watchPositionAsync } from "expo-loca
 
 export default (shouldTrack, callback) => {
   const [error, setError] = useState(undefined);
-  const [subscriber, setSubscriber] = useState(null);
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10
-        },
-        callback
-      );
-      setSubscriber(sub);
-    } catch (e) {
-      setError(e);
-    }
-  };
 
   useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestPermissionsAsync();
+        if (!granted) {
+          throw new Error('Location permission not granted');
+        }
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10
+          },
+          callback
+        );
+      } catch (e) {
+        setError(e);
+      }
+    };
+
     if (shouldTrack) {
       startWatching().then();
     } else {
-      subscriber.remove();
-      setSubscriber(null);
+      if (subscriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
     }
 
     return () => {
