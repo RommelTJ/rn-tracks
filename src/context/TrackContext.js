502942a086ea97f9160250f65c86ceae3c574143
Rommel Rico <me@rommelrico.com>
2020-07-16 07:11:01 -0700

    :tada: Tracks: TrackContext

---
+++ src/context/TrackContext.js
@@ -0,0 +1,26 @@
+import createDataContext from "./createDataContext";
+
+const trackReducer = (state, action) => {
+  switch (action.type) {
+    case "fetch_tracks":
+      return state;
+    case "create_track":
+      return state;
+    default:
+      return state;
+  }
+};
+
+const fetchTracks = (dispatch) => () => {
+  dispatch({ type: 'fetch_tracks' });
+};
+
+const createTrack = (dispatch) => () => {
+  dispatch({ type: 'create_track' });
+};
+
+export const { Context, Provider } = createDataContext(
+  trackReducer,
+  { fetchTracks, createTrack },
+  []
+);
