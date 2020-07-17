beda7ceceb7b7ef6f133b3810970dd700862195c
Rommel Rico <me@rommelrico.com>
2020-07-17 07:15:54 -0700

    :sparkles: Tracks: Fetching Created Tracks

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -4,21 +4,19 @@ import trackerApi from "../api/tracker";
 const trackReducer = (state, action) => {
   switch (action.type) {
     case "fetch_tracks":
-      return state;
-    case "create_track":
-      return state;
+      return action.payload;
     default:
       return state;
   }
 };
 
-const fetchTracks = (dispatch) => () => {
-  dispatch({ type: 'fetch_tracks' });
+const fetchTracks = (dispatch) => async () => {
+  const response = await trackerApi.get('/tracks');
+  dispatch({ type: 'fetch_tracks', payload: response.data });
 };
 
 const createTrack = (dispatch) => async (name, locations) => {
-  const x = await trackerApi.post('/tracks', { name, locations });
-  dispatch({ type: 'create_track' });
+  await trackerApi.post('/tracks', { name, locations });
 };
 
 export const { Context, Provider } = createDataContext(
