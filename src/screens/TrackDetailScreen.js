cc86ffa13d4e91b58d2ff617fba1665d3d5bd446
Rommel Rico <me@rommelrico.com>
2020-07-17 07:33:28 -0700

    :sparkles: Tracks: Showing Track Details

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import MapView, { Polyline } from "react-native-maps";
+
 import { Context as TrackContext } from "../context/TrackContext";
 
 const TrackDetailScreen = (props) => {
@@ -7,14 +9,23 @@ const TrackDetailScreen = (props) => {
   const { state } = useContext(TrackContext);
   const _id = navigation.getParam('_id');
   const track = state.find(t => t._id === _id);
+  const initialCoords = track.locations[0].coords;
 
   return (
-    <View>
+    <>
       <Text style={{fontSize: 48}}>Track Detail Screen: {track.name}</Text>
-    </View>
+      <MapView
+        initialRegion={{ longitudeDelta: 0.01, latitudeDelta: 0.01, ...initialCoords }}
+        style={styles.map}
+      >
+        <Polyline coordinates={track.locations.map(l => l.coords)} />
+      </MapView>
+    </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: { height: 300 }
+});
 
 export default TrackDetailScreen;
