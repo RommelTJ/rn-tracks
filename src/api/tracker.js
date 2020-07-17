fde7719919214e314ec2bbe4920ede8f464566a1
Rommel Rico <me@rommelrico.com>
2020-07-17 07:04:04 -0700

    :sparkles: Tracks: Automatic Authentication

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,21 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import ENV from '../../env';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: ENV().baseURL
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default instance;
