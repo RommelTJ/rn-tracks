aafca4ad71ea19cd9e4c073ccfe22b61586e6ffd
Rommel Rico <me@rommelrico.com>
2020-07-17 07:41:31 -0700

    :sparkles: Tracks: Fixing odds and ends

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, Button } from 'react-native-elements';
 import { SafeAreaView } from "react-navigation";
+import { FontAwesome } from "@expo/vector-icons";
+
 import { Context as AuthContext } from "../context/AuthContext";
 import Spacer from "../components/Spacer";
 
@@ -13,6 +15,11 @@ const AccountScreen = () => {
       <Spacer><Button title="Sign Out" onPress={signout} /></Spacer>
     </SafeAreaView>
   );
+};
+
+AccountScreen.navigationOptions = {
+  title: "Account",
+  tabBarIcon: <FontAwesome name="gear" size={20} />
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { ListItem } from "react-native-elements";
 import { NavigationEvents } from "react-navigation";
 import { Context as TrackContext } from "../context/TrackContext";
@@ -11,7 +11,6 @@ const TrackListScreen = (props) => {
   return (
     <>
       <NavigationEvents onWillFocus={fetchTracks} />
-      <Text style={{fontSize: 48}}>Track List Screen</Text>
       <FlatList
         data={state}
         keyExtractor={(item) => item._id}
@@ -25,6 +24,10 @@ const TrackListScreen = (props) => {
       />
     </>
   );
+};
+
+TrackListScreen.navigationOptions = {
+  title: 'Tracks'
 };
 
 const styles = StyleSheet.create({});
